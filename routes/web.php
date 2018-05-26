65ff1fd1d5f9b7e13bee23a873c99700b43bca99
Amr Hassan <amrh434@gmail.com>
2018-05-26 16:10:18 +0200

    feat : show single Thread  by id  .

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/threads', 'ThreadController@index');
+Route::get('/threads/{thread}', 'ThreadController@show');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
