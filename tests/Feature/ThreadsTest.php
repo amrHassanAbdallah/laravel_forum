9ed72ae826db7979ca914a02aaa453cf94142a1d
Amr Hassan <amrh434@gmail.com>
2018-05-26 16:05:55 +0200

    test : show single Thread test by id  .

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -24,5 +24,8 @@ class ThreadsTest extends TestCase
         $response = $this->get('/threads');
         $response->assertStatus(200);
         $response->assertSee($thread->title);
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
     }
 }
