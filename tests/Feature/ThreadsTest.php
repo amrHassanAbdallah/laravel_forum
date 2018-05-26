5249bc4401389f0b85d2303d97067c7528129929
Amr Hassan <amrh434@gmail.com>
2018-05-26 16:02:45 +0200

    test : Thread index test to see the threads and response status 200.

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->seed('ThreadsSeeder');
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_browse_threads()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads');
+        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+    }
+}
