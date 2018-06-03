2444d0fe693e907c8d8459fa4a4345ab5aa248e7
Amr Hassan <amrh434@gmail.com>
2018-06-03 22:50:48 +0200

    feat : add reply  form for signed in users .

--- resources/views/threads/single.blade.php
+++ resources/views/threads/single.blade.php
@@ -16,7 +16,28 @@
             @foreach($thread->replies as $reply)
                 @include('threads.reply')
             @endforeach
+
+            <div class="col-md-8" style="margin-top: 10px">
+                @if(auth()->check())
+                    <form action="{{route('replies.store',$thread->id)}}" method="post">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <textarea style="width: 100%" name="body" id="body" placeholder="Have something to say ?"
+                                      rows="5"></textarea>
+                        </div>
+
+                        <button class="btn btn-default" type="submit">post</button>
+                    </form>
+
+                @else
+                    <p class="text-center"> Please <a href="{{route('login')}}">sign in</a> to participate in this
+                        discussion.</p>
+
+                @endif
+            </div>
         </div>
+
+
     </div>
 
 @endsection
